39c82ec25814b0383146dbd1ecf237e609bb9b2d
Adrian Fernandez <adrianfersa@meta.com>
2023-06-14 12:04:53 -0700

    added comunication between api and my js

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 
-const apikey = '2eLtc80qA0rO0I5pBNzcelF8wEo5NPOP';
+const api = 'https://api.giphy.com/v1/gifs/search?api_key=2eLtc80qA0rO0I5pBNzcelF8wEo5NPOP&q=%22cats%22';
 
 var submitBtn = document.getElementById("submitBtn");
 
 submitBtn.addEventListener("click", loadContent);
 
-function loadContent(event) {
-    event.preventDefault()
+async function loadContent(event) {
+  event.preventDefault();
 
-    console.log("hi")
+  try {
+    const response = await fetch(api);
+    const data = await response.json();
+
+    // Process the response data
+    console.log(data);
+    console.log(data.data[0])
+    // Your code to handle the data goes here
+  } catch (error) {
+    // Handle any errors that occur during the request
+    console.error('Error:', error);
+  }
 }
+
+
+
+
+
+// https://api.giphy.com/v1/gifs/search?api_key=2eLtc80qA0rO0I5pBNzcelF8wEo5NPOP&q=%22cats%22
