5efa1dcdb1fbda3ccf88ff25590710f1f60fdbf8
Adrian Fernandez <adrianfersa@meta.com>
2023-06-14 12:42:18 -0700

    added gifs and you can now show them

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-
-const api = 'https://api.giphy.com/v1/gifs/search?api_key=2eLtc80qA0rO0I5pBNzcelF8wEo5NPOP&q=%22cats%22';
-
 var submitBtn = document.getElementById("submitBtn");
+var gifs = document.getElementById("gifs");
+var searchInput = document.getElementById("searchInput");
 
 submitBtn.addEventListener("click", loadContent);
 
 async function loadContent(event) {
   event.preventDefault();
+
+  const apiKey = '2eLtc80qA0rO0I5pBNzcelF8wEo5NPOP';
+  const query = searchInput.value;
+  const api = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${encodeURIComponent(query)}`;
 
   try {
     const response = await fetch(api);
@@ -14,13 +17,22 @@ async function loadContent(event) {
 
     // Process the response data
     console.log(data);
-    console.log(data.data[0])
-    // Your code to handle the data goes here
+    console.log(data.data[0]);
+
+    // Clear previous search results
+    gifs.innerHTML = '';
+
+    data.data.forEach(gif => {
+      const image = document.createElement("img");
+      image.src = gif.images.original.url;
+      gifs.appendChild(image);
+    });
   } catch (error) {
     // Handle any errors that occur during the request
     console.error('Error:', error);
   }
 }
+
 
 
 
