a9c3c2dd01f71a99a8c371f0a9791a28d00ba386
Adrian Fernandez <adrianfersa@meta.com>
2023-06-14 11:17:56 -0700

    added testing submit button and added a little of UI, also added the key for the api

---
+++ app.js
@@ -0,0 +1,12 @@
+
+const apikey = '2eLtc80qA0rO0I5pBNzcelF8wEo5NPOP';
+
+var submitBtn = document.getElementById("submitBtn");
+
+submitBtn.addEventListener("click", loadContent);
+
+function loadContent(event) {
+    event.preventDefault()
+
+    console.log("hi")
+}
